368e0458f99885dff4897094c5e9097773993e44
jy617lee <jy617.lee@gmail.com>
2025-11-20 16:22:00 +0900

    refactor: 중복된 주석 제거

--- TimezoneAlarm/Views/DeviceModeNotificationHandler.swift
+++ TimezoneAlarm/Views/DeviceModeNotificationHandler.swift
@@ -2,7 +2,6 @@
 /// 앱이 활성화될 때마다 기기 모드를 확인하고, 필요시 사용자에게 알림을 표시합니다.
 import SwiftUI
 
-/// 무음모드/방해금지모드 체크 및 팝업 표시를 담당하는 ViewModifier
 struct DeviceModeNotificationModifier: ViewModifier {
     @State private var deviceModeToShow: DeviceModeState? = nil
     
